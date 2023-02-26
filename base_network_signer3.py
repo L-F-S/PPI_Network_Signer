5db07592216d19bab7a30a3eae6e8189a7cc4637
L-F-S <lorenzos@mail.tau.ac.il>
2023-02-26 17:13:27 +0200

    pytorch - cuda implementation test

---
+++ base_network_signer3.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+import os
+import argparse
+import pickle
+import pandas as pd
+import networkx
+from time import time
+from preproc_utils import add_edges_from_labels, graph_from_dataframe,\
+    load_training_data
+from score_edges_gpu import generate_similarity_matrix, gpu_generate_features
+
+###############################################################################
+#   INPUTS
+###############################################################################
+parser = argparse.ArgumentParser()
+parser.add_argument('datasets', type=str, nargs='*',
+                    help='dataset names:  options (and default) for S_cerevisiae: [\'kegg\',\'kpi\', \'ubiq\'],\
+                        options (anmd default) for H_sapiens: [\'kegg\',\'PSP\',\'depod\',\'ubinet2\']')
+parser.add_argument('-c', dest='N_JOBS', type=int, nargs='?', default=8,
+                    help='number of corse to assign')
+parser.add_argument('-e', dest='edges', type=str, nargs='?', default=None,
+                    help='Optional. Network edges to create features for.\
+                        Options: None: creates features for training edges only\
+                        \'all\': creates features for all edges in the PPI network,\
+                        \'validation\': creates features for test edges only (i.e. not in training data),\
+                        <fiilename>: creates features for pickled list of tuples corresponding to edge names.\
+                        Edges must be part of base PPI network.')
+parser.add_argument('-s', dest='SPECIES', type=str, nargs='?', default='S_cerevisiae',
+                    help='species: [\'H_sapiens\', \'S_cerevisiae\']\ndefault: S_cerevisiae')
+parser.add_argument('-p', dest='perturbation_filename', type=str, nargs='?', default='Holstege',
+                    help='default: \'Holstege\' , other options: [\'reimand\', ADPBH, CMGE..]')
+args = parser.parse_args()
+
+N_JOBS = args.N_JOBS
+SPECIES = args.SPECIES
+if len(args.datasets) == 0:
+    datasets = ['kegg','kpi', 'ubiq'] if SPECIES == 'S_cerevisiae' else ['kegg','PSP','depod','ubinet2']
+else:
+    datasets = args.datasets
+print(N_JOBS, datasets)
+print(SPECIES)
+
+HOME_DIR  =  '/home/bnet/lorenzos/signed/signedv3/'
+#HOME_DIR  =  "G:" +os.sep+"My Drive"+ os.sep +"SECRET-ITN" + os.sep +"Projects" + os.sep 
+
+DATA_DIR=HOME_DIR+"input"+os.sep+SPECIES+os.sep
+#DATA_DIR=HOME_DIR+"Data"+os.sep+SPECIES+os.sep
+DIRECTED_DIR = HOME_DIR+"network_signing"+os.sep
+OUTDIR=HOME_DIR+'output'+os.sep+SPECIES+os.sep
+
+# Propagation parameters:
+PROPAGATE_ALPHA = 0.6
+PROPAGATE_EPSILON = 1e-5
+PROPAGATE_ITERATIONS = 100
+
+###############################################################################
+#     Load training sets:
+###############################################################################
+print("\n>>>>>>> loading signed edges datasets:")
+signed_datasets_labels, signed_datasets_edge_weights = load_training_data(DATA_DIR, datasets, SPECIES)
+
+###############################################################################
+#     Load PPI network:
+###############################################################################
+print("\n>>>>>>> loading base network to propagte on:")
+graph = graph_from_dataframe(DATA_DIR, SPECIES)
+#ADD edges from training labels which are not present:
+
+print("\n>>>>>>> adding missing edges from signed data to base network:")
+for dataname, signed_data in signed_datasets_edge_weights.items():
+    graph = add_edges_from_labels(graph, signed_data, v=False)
+
+###############################################################################
+#     Load perturbation maps:
+###############################################################################
+
+genes = sorted(graph.nodes)
+
+perturbations_name=args.perturbation_filename
+with open(DATA_DIR+'plus_targets_'+perturbations_name+'.pkl', 'rb') as f:
+    plus_targets_of_deletion = pickle.load(f)
+with open(DATA_DIR+'minus_targets_'+perturbations_name+'.pkl', 'rb') as f:
+    minus_targets_of_deletion = pickle.load(f)
+
+print("\n--------------- INPUT DATA ---------------")
+print('- Positive knockout experiment targets:', len(plus_targets_of_deletion),'\n- Negative knockout experiment targets:',len(minus_targets_of_deletion))
+print('- Labels (0 for positive and 1 for negative signe of directed interaction):')
+for name, label in signed_datasets_labels.items():
+    print('\t',name,':')
+    for i in label.value_counts().index:
+        print('\t\t'+str(i)+'\t'+str(label.value_counts()[i]))
+print('- Base network size: ','\n\t',len(graph.edges()),'edges','\n\t',len(graph.nodes()),'nodes')
+#%% Select edges to use
+        
+if not args.edges:
+    edges_list = [d.index for d in signed_datasets_labels.values()]
+    names=list(signed_datasets_labels.keys())
+elif args.edges == 'all':
+    edges_list = [list(graph.edges)]
+    names=['all']
+elif args.edges == 'valid':
+    edges = list(set(graph.edges)-set(pd.concat(list(signed_datasets_labels.values())).index))
+    edges_list = [edges]
+    names=['validation']
+else:
+    names=[args.edges]
+    with open(DATA_DIR+args.edges,'rb') as f:
+        edges=pickle.load(f)
+    edges_list=[edges]
+print('- # edges to create features for: ', sum([len(edges) for edges in edges_list]))
+print('------------------------------------------')
+#%%
+###############################################################################
+#    Generate similarity matrix:
+###############################################################################
+print('Generating similarity matrix.')
+raw_matrix = networkx.to_scipy_sparse_matrix(graph, genes, format='csc').T
+matrix, raw_col_sums = generate_similarity_matrix(raw_matrix) #normalized similarity matrix
+num_genes     = len(genes)
+gene_indexes  = dict([(gene, index) for (index, gene) in enumerate(genes)]) #all genes present in matrix
+###############################################################################
+#%%    Generate features (the slow part!):
+###############################################################################
+print('Generating features...')
+for name, edges in zip(names, edges_list):
+    start=time()
+    knockout_names, results = gpu_generate_features(raw_matrix, edges, gene_indexes, matrix, plus_targets_of_deletion,\
+                          minus_targets_of_deletion,  num_genes, PROPAGATE_ALPHA, \
+                              PROPAGATE_ITERATIONS,PROPAGATE_EPSILON )
+
+    print("time passed", time()-start)
+    data = pd.DataFrame().from_records(results, index=[0,1], columns = knockout_names[0])
+    data.to_csv(OUTDIR+name+'_'+perturbations_name+'.ft.csv')
+    
+    #todo add saving csv to pickle format for faster loading later
+
---
+++ score_edges_gpu.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  4 11:33:35 2021
+
+@author: los4
+
+linear algebra operations on pytorch vectors
+"""
+
+import numpy  as np
+import scipy
+import math
+import warnings
+warnings.filterwarnings("ignore")
+
+
+###############################################################################
+# set pytorch environment
+###############################################################################
+import torch
+
+DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+###############################################################################
+#  GENERATE FEATURES
+###############################################################################
+
+
+# Simple propagation:
+    
+    
+def generate_similarity_matrix(matrix):
+    """Normalize a similarity matrix
+    by number of outgoing edges from node.
+    this normalization works for  both
+    directed and undirected graphs
+    inputs:
+        matrix: Scipy.sparse matrix 
+    outputs:
+        matrix: 1-D torch.tensor
+        sum_vector: 1-D torch.tensor
+        """
+    matrix = matrix.toarray()
+    matrix = torch.from_numpy(matrix).type(torch.float).to(DEVICE) #to_sparse(). 
+    sum_vector =  matrix.sum(axis=0) #torch.sparse.sum(matrix, 0) # sum over rows (compress columns), (to_dense xke non posso assign values a sparse tensors (operazione sotto))
+    sum_vector[sum_vector == 0] = 1  #substitute 0s with ones , to avoid division by 0 later
+    norm_matrix = torch.diag(1/sum_vector) # create a diagonal matrix (all 0s except diagonal) with 1/sum vector
+    matrix = matrix @ norm_matrix  #multiply the two (matrix multiplication). (effectively u are dividing each column by the sum of the column).
+
+    return  matrix, sum_vector 
+
+
+def propagate(PROPAGATE_ALPHA, PROPAGATE_ITERATIONS, PROPAGATE_EPSILON, seeds, matrix, gene_indexes, num_genes):
+    '''propagate from seeds inside matrix
+    gene indexes is a dictinary mapping gene names to their indexes in matrix'''
+    
+    # matrix = np.array(matrix)x
+    F_t = torch.zeros(num_genes).to(DEVICE)
+    F_t[[gene_indexes[seed] for seed in seeds if seed in gene_indexes]] = 1
+    Y = (1 - PROPAGATE_ALPHA) * F_t
+
+    for _ in range(PROPAGATE_ITERATIONS):
+        F_t_1 = F_t  #05 045 2022v add transpose perche adesso matrix e  una np.matrix e bo se no mi da errore
+        F_t =  PROPAGATE_ALPHA*matrix.matmul(F_t_1) + Y 
+        
+        if math.sqrt(torch.norm(F_t_1 - F_t)) < PROPAGATE_EPSILON:
+            break
+    return F_t
+
+# Edge-defective propagation:
+
+
+def extract_and_normalize_column_of(matrix, ind1, ind2):
+    #05 04 added intexing [0] to return perche non e piu sparse amtrix ma numpy.matrix
+    col1=matrix[:,ind1].copy()
+    col2=matrix[:,ind2].copy()
+    col1[ind2]=0
+    col2[ind1]=0
+    
+    return col1/col1.sum() if col1.sum() !=0 else 1, col2/col2.sum() if col2.sum() !=0 else 1
+
+
+def create_defective_columns(non_normalized_sparse_matrix, edge, gene_indexes):
+    """
+    takes a column-normalzied sparse matrix, normalized with A D^-1
+    and removes one interaction from it, but keeps the normalization,
+    effectlively re-normalizing two columns.
+    inputs: scipy sparse matrix (csr), tuple, dictionary.
+    """
+    ind1=gene_indexes[edge[0]]
+    ind2=gene_indexes[edge[1]]
+    if ind1>ind2:
+        temp=ind2
+        ind2=ind1
+        ind1=temp
+    column1, column2=extract_and_normalize_column_of(non_normalized_sparse_matrix, ind1, ind2)
+    return column1, column2, ind1, ind2
+
+
+def defective_norm_matrix_from_columns(normalized_sparse_matrix, col1, col2, ind1, ind2):
+
+    """takes a column-normalzied sparse matrix, normalized with A D^-1
+    and removes one interaction from it, but keeps the normalization, effectlively
+    re-normalizing two columns."""
+    
+    def_matrix=normalized_sparse_matrix.copy()
+    def_matrix[:,ind1]=col1
+    def_matrix[:,ind2]=col2
+    return def_matrix
+
+
+def score(prop_all, prop_noedge,positive_foldchange_genes_indexes, negative_foldchange_genes_indexes=None, edge=None):
+    # current score: Summ the differences of propagation scores (between normal 
+    # vs defective network) of all positive foldchanges divided
+    #  by sum of difference of propoagation scores of all negatively affected genes with a normalization
+# normalizzare attraverso il sum(edge positive)/n(edge positive) *n/edge negative
+    # e un modo brutto di scrivere AVg(Delta(P=+))/ Avg(delta(P-))
+    '''returns a float'''
+    
+    if negative_foldchange_genes_indexes: #todo 01/09/2021 currently deprecated
+        try:
+            norm = len(prop_all[negative_foldchange_genes_indexes])*(sum(prop_all[positive_foldchange_genes_indexes] - prop_noedge[positive_foldchange_genes_indexes]))/len(prop_all[positive_foldchange_genes_indexes])
+            return sum(prop_all[negative_foldchange_genes_indexes] - prop_noedge[negative_foldchange_genes_indexes])/norm
+        except:
+            raise ValueError('Empty list of gene targets',positive_foldchange_genes_indexes, edge)
+
+    else: # actually using this. positive_foldchange_genes_indexes can actually be negative, depending on what you input
+        try:
+            return sum(prop_all[positive_foldchange_genes_indexes]-prop_noedge[positive_foldchange_genes_indexes])/len(positive_foldchange_genes_indexes)
+        except:
+            raise ValueError('Empty list of gene targets',positive_foldchange_genes_indexes, edge)
+
+
+def gpu_generate_features(raw_matrix, edges, gene_indexes, matrix, plus_targets_of_deletion, minus_targets_of_deletion, num_genes, PROPAGATE_ALPHA, PROPAGATE_ITERATIONS, PROPAGATE_EPSILON):
+    '''fills one row of features for one sample (edge) for the features matrix to feed to the classifier.
+    
+    raw_matrix: the base network matrix
+    matrix: normalized similarity matrix by number of outgoing edges
+    edge: tuple of ints. ints are the nodes IDs. the training edge with known orientation. Also, the edge to be removed to create each feature
+    gene_indexes: dict of geneid:index in the genes list (outside this function)
+    plus/minus_targets_of_deletion: dict. {knockoutgene:list of target positive(?negative) genes}
+    num genes: int. number of genes
+    PROPAGATE_x: ints. propagation parameters
+    returns:
+        knockout_names:  list [0,1,knockout1+,knockout2+...knockoutn+,
+                               knockout1-,...knockoutn-]
+        score_of: list  [edge1ID,edge2ID, score1,....score2n]
+    '''
+    shape=(len(edges),len(plus_targets_of_deletion.keys())*2)
+    # tensorone = torch.zeros(shape).to(DEVICE)
+    # tensorone = fill_tensor_with_function(tensorone, function)
+    
+    
+    for edge in edges:
+        # col1, col2, ind1, ind2 = create_defective_columns(raw_matrix, edge, gene_indexes)
+        defective_normalized_matrix = matrix#defective_norm_matrix_from_columns(matrix, col1, col2,ind1, ind2 )
+    
+        score_of=[edge[0], edge[1]]
+        knockout_names = [0,1]
+        
+        for n, source in enumerate(plus_targets_of_deletion.keys()):
+                print(n)
+                prop_edge_fwd = propagate(PROPAGATE_ALPHA, PROPAGATE_ITERATIONS, PROPAGATE_EPSILON, {source},matrix,gene_indexes,num_genes)  
+                # create defective matrix and run defective propagation:
+                prop_noedge_fwd = propagate(PROPAGATE_ALPHA, PROPAGATE_ITERATIONS, PROPAGATE_EPSILON,{source}, defective_normalized_matrix,gene_indexes,num_genes) # num_genes is the same.
+                score_of.append(score(prop_edge_fwd, prop_noedge_fwd,[gene_indexes[x] for x in plus_targets_of_deletion[source]],edge=edge)) # [gene_indexes[x] for x in pplus_targets_of_deletio] is substituting node names with their relative index in the list
+                knockout_names.append(str(source)+'+')
+                
+    
+        for n,source in enumerate(minus_targets_of_deletion.keys()): #01/03/2022 TODO prop_edge_fwd e prop_noedge_fwd in questo ciclo non serve farli, per come sono plus?minus_targets of deletion ora, perche' hanno le stesse keys per ora, xke prendo solo knockouts che hanno sia plus che minus targets dentro, quindi per ora questa propagazione e' ridondante.
+    
+                prop_edge_fwd = propagate(PROPAGATE_ALPHA, PROPAGATE_ITERATIONS, PROPAGATE_EPSILON, {source},matrix,gene_indexes,num_genes) 
+                # create defective matrix and run defective propagation:
+                prop_noedge_fwd = propagate(PROPAGATE_ALPHA, PROPAGATE_ITERATIONS, PROPAGATE_EPSILON, {source}, defective_normalized_matrix, gene_indexes,num_genes) # num_genes is the same.            
+                score_of.append(score(prop_edge_fwd, prop_noedge_fwd,[gene_indexes[x] for x in minus_targets_of_deletion[source]], edge=source)) 
+                knockout_names.append(str(source)+'-')
+    
+        return knockout_names, score_of 
+
+
+
+
+
+#04 - 04 - 2022  :
+    # cambiato metodo di creazione defective matrix ma e' molto piu lento nn so xke
+    #quindi nisba
+
+
+def create_defective_matrix2(normalized_sparse_matrix, raw_matrix_column_sum_1, raw_matrix_column_sum_2, ind_1, ind_2, raw_weight_1, raw_weight_2):
+    '''
+    Column sum: int, s
+    '''
+    def_matrix=normalized_sparse_matrix.copy()
+    
+    # set edge weight to 0 (works for both directed and undirected graphs)
+    def_matrix[ind_2,ind_1] = 0
+    def_matrix[ind_1,ind_2] = 0
+
+    def_matrix[:,ind_1] = def_matrix[:,ind_1]*(raw_matrix_column_sum_1/(raw_matrix_column_sum_1-raw_weight_1)) 
+    def_matrix[:,ind_2] = def_matrix[:,ind_2]*(raw_matrix_column_sum_2/(raw_matrix_column_sum_2-raw_weight_2))
+    
+    return def_matrix
+
+def get_indexes_and_columns2(raw_matrix, gene_indexes, raw_col_sums, edge):
+    '''
+    note: raw_matrix[ind2,ind1] takes the weight of the edge, from the column 
+    of ind1, so given our nomenclature, it will be edge_weight_1
+    '''
+    
+    ind1=gene_indexes[edge[0]]
+    ind2=gene_indexes[edge[1]]
+    return ind1, ind2, raw_matrix[ind2,ind1], raw_matrix[ind1, ind2], raw_col_sums[ind1], raw_col_sums[ind2]
+
+def create_the_features_different_knockouts2(raw_matrix, edge, gene_indexes, matrix, plus_targets_of_deletion, minus_targets_of_deletion, num_genes, raw_col_sums, PROPAGATE_ALPHA, PROPAGATE_ITERATIONS, PROPAGATE_EPSILON):
+    #04 04 2022 NON e' piu  veloce...
+    '''
+    fills one row of features for one sample (edge) for the features matrix to feed to the classifier.
+    raw_matrix: the base network matrix
+    matrix: normalized similarity matrix by number of outgoing edges
+    edge: tuple of ints. ints are the nodes IDs. the training edge with known orientation. Also, the edge to be removed to create each feature
+    gene_indexes: dict of geneid:index in the genes list (outside this function)
+    plus/minus_targets_of_deletion: dict. {knockoutgene:list of target positive(?negative) genes}
+    raw_col_sums: list of sum over non normalized columns (no 0 values)
+    num genes: int. number of genes
+    PROPAGATE_x: ints. propagation parameters
+    '''
+
+    tic1 = time.time()
+    ind_1, ind_2, edge_weight_1, edge_weight_2, col_sum1, col_sum2 = get_indexes_and_columns2(raw_matrix, gene_indexes, raw_col_sums, edge)
+    tic2 = time.time()
+    defective_normalized_matrix = create_defective_matrix2(matrix, col_sum1, col_sum2, ind_1, ind_2, edge_weight_1, edge_weight_2)
+    tic3 = time.time()
+    
+
+    
+    score_of=[edge[0], edge[1]]
+
+    for n, source in enumerate(plus_targets_of_deletion.keys()):
+        if n<5000:  # small numbers to not iterate over all knockouts
+            
+            prop_edge_fwd = propagate(PROPAGATE_ALPHA, PROPAGATE_ITERATIONS, PROPAGATE_EPSILON, {source},matrix,gene_indexes,num_genes)          
+            # create defective matrix and run defective propagation:
+            prop_noedge_fwd = propagate(PROPAGATE_ALPHA, PROPAGATE_ITERATIONS, PROPAGATE_EPSILON,{source}, defective_normalized_matrix,gene_indexes,num_genes) # num_genes is the same.
+            score_of.append(score(prop_edge_fwd, prop_noedge_fwd,[gene_indexes[x] for x in plus_targets_of_deletion[source]],edge=edge)) # [gene_indexes[x] for x in pplus_targets_of_deletio] is substituting node names with their relative index in the list
+
+
+    for n,source in enumerate(minus_targets_of_deletion.keys()): #01/03/2022 TODO prop_edge_fwd e prop_noedge_fwd in questo ciclo non serve farli, per come sono plus?minus_targets of deletion ora, perche' hanno le stesse keys per ora, xke prendo solo knockouts che hanno sia plus che minus targets dentro, quindi per ora questa propagazione e' ridondante.
+        if n<5000: # small numbers to not iterate over all knockouts
+
+            prop_edge_fwd = propagate(PROPAGATE_ALPHA, PROPAGATE_ITERATIONS, PROPAGATE_EPSILON, {source},matrix,gene_indexes,num_genes) 
+            # create defective matrix and run defective propagation:
+            prop_noedge_fwd = propagate(PROPAGATE_ALPHA, PROPAGATE_ITERATIONS, PROPAGATE_EPSILON, {source}, defective_normalized_matrix, gene_indexes,num_genes) # num_genes is the same.            
+            score_of.append(score(prop_edge_fwd, prop_noedge_fwd,[gene_indexes[x] for x in minus_targets_of_deletion[source]], edge=source)) 
+
+    return score_of #1/3/2022 al momentp e' una lista con [edge1ID,edge2ID, score1,....score472]
