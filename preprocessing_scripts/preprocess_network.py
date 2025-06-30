a35fe8d81f71cb8d1a94275a158ed42920fb433b
L-F-S <lorenzo.fsignorini@outlook.it>
2025-06-30 16:31:14 +0200

    Updated paths

--- preprocessing_scripts/preprocess_network.py
+++ preprocessing_scripts/preprocess_network.py
@@ -2,64 +2,36 @@
 """
 Created on Fri Oct 20 16:38:44 2023
 
-@author: los4
+@author: L-F-S
 
-created to run once to potentially add training label data to 
-base network. to run before SIGNAL feature generation
+to run before SIGNAL feature generation
+Run once to add training label data to 
+base network (if needed) and
+convert network into pickled binary 
 """
-import os
-import argparse
 import pickle
-import pandas as pd
-import networkx
-from time import time
-from preproc_utils import add_edges_from_labels, read_network_from_file,graph_from_dataframe,\
+from preproc_utils import add_edges_from_labels, graph_from_dataframe,\
     load_training_data
-from score_edges import generate_similarity_matrix, create_the_features_different_knockouts_iterative
 import networkx as nx
+from glob_vars import SPECIES, TRAIN_DATA, LBL_DIR, NET_DIR
 
 ###############################################################################
 #   INPUTS
 ###############################################################################
-SPECIES ='S_cerevisiae'
-datasets = ['patkar_kegg','kpi', 'ubiq'] if SPECIES == 'S_cerevisiae' else ['kegg','PSP','depod','ubinet2']
 
-print(datasets)
+print(TRAIN_DATA)
 print(SPECIES)
-
-# # Servier directories:
-# HOME_DIR  =  '/home/bnet/lorenzos/signed/signedv3/'
-# INPUT_DIR=HOME_DIR+"input"+os.sep+SPECIES+os.sep
-# OUTDIR=HOME_DIR+'output'+os.sep+SPECIES+os.sep+'validation_out'
-# FEATURESDIR=OUTDIR
-# BASENETDIR=INPUT_DIR
-
-# Local directories
-HOME_DIR  =  "G:" +os.sep+"My Drive"+ os.sep +"SECRET-ITN" + os.sep +"Projects" + os.sep 
-# HOME_DIR  =  "G:" +os.sep+"Il mio Drive"+ os.sep +"SECRET-ITN" + os.sep +"Projects" + os.sep 
-SIGNAL_DIR = HOME_DIR+"network_signing"+os.sep
-os.chdir(SIGNAL_DIR)
-DATA_DIR = HOME_DIR+'Data'+os.sep+SPECIES+os.sep
-INPUT_DIR=SIGNAL_DIR+'features_and_labels'+os.sep+SPECIES+os.sep
-networkdir=SIGNAL_DIR+'input'+os.sep+SPECIES+os.sep+'network'+os.sep
-
-BASENETDIR=DATA_DIR
-
-# Propagation parameters:
-PROPAGATE_ALPHA = 0.6
-PROPAGATE_EPSILON = 1e-5
-PROPAGATE_ITERATIONS = 100
 
 ###############################################################################
 #     Load training sets:
 ###############################################################################
 print("\n>>>>>>> loading signed edges datasets:")
-signed_datasets_labels, signed_datasets_edge_weights = load_training_data(INPUT_DIR, datasets, SPECIES)
+signed_datasets_labels, signed_datasets_edge_weights = load_training_data(LBL_DIR, TRAIN_DATA, SPECIES)
 ###############################################################################
 #     Load PPI network:
 ###############################################################################
 print("\n>>>>>>> loading base network to propagte on:")
-graph = graph_from_dataframe(BASENETDIR, SPECIES)
+graph = graph_from_dataframe(NET_DIR, SPECIES)
 #ADD edges from training labels which are not present:
 #%%
 print("\n>>>>>>> adding missing edges from signed data to base network:")
@@ -67,6 +39,6 @@ for dataname, signed_data in signed_datasets_edge_weights.items():
     graph = add_edges_from_labels(graph, signed_data, v=False)
 
 #%%
-datasetstr='_'.join(datasets)
-with open(networkdir+SPECIES+'_'+datasetstr+'.nx', 'wb') as f:
+datasetstr='_'.join(TRAIN_DATA)
+with open(NET_DIR+SPECIES+'_'+datasetstr+'2.nx', 'wb') as f:
     pickle.dump(graph,f)
